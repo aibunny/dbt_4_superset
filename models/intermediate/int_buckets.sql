87ea616fd311c3dab68909145e6b0b3b3c032935
aibunny <waihenyafredrick@gmail.com>
2024-02-12 10:34:03 +0300

    created intermediate models for teams and buckets

---
+++ models/intermediate/int_buckets.sql
@@ -0,0 +1,19 @@
+with buckets as (
+    select
+        id as bucket_id,
+        title,
+        description,
+        product_id,
+        sub_product_id,
+        lower_limit,
+        upper_limit,
+        active,
+        days_range,
+        created_at,
+        deleted_at
+    
+    from {{ ref('stg_smartcollect__buckets') }}
+
+)
+
+select * from buckets where deleted_at is null and active = True
---
+++ models/intermediate/int_teams.sql
@@ -0,0 +1,18 @@
+with teams as (
+    select
+        id as team_id,
+        organization_id,
+        branch_id,
+        agency_id,
+        title,
+        active,
+        team_type,
+        team_leader as team_leader_id,
+        created_by,
+        created_at,
+        deleted_at
+    from {{ ref('stg_smartcollect__teams') }}
+)
+
+
+select * from teams where deleted_at is null and active = True
