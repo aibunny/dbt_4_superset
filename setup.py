d9275895a89263636d6f9da13e4f7c50e0283b1f
aibunny <waihenyafredrick@gmail.com>
2024-02-20 12:16:39 +0300

    added push metric logic

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = 'dbt-4-superset',
+    version='1.0.0',
+    author='AIBUNNY',
+    author_email='fred@crafted.co.ke',
+    description="Allows automation between superset and dbt",
+    long_description=long_description,
+    long_description_content_type = "text/markdown",
+    url='https://github.com/aibunny/dbt-4-superset',
+    project_urls = {
+        "Bug Tracker": "https://github.com/aibunny/dbt-4-superset/issues"
+    },
+    license='MIT',
+    packages=['toolbox'],
+    install_requires=['requests'],
+)
