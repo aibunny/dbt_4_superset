0655e4bfddf5f6cb16a15b918f32c14350a42b4e
aibunny <waihenyafredrick@gmail.com>
2024-03-11 13:19:04 +0300

    Created entrypoint and updated docs

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='smartcollect-lineage',
-    version='0.1.0',
+    version='0.2.0',
     author='AIBUNNY',
-    author_email='analytics@crafted.co.ke',
+    author_email='fred@crafted.co.ke',
     description="Allows automation between superset and dbt",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/aibunny/dbt-4-superset',
+    url='https://github.com/crafted-systems/smartcollect-lineage',
     project_urls={
-        "Bug Tracker": "https://github.com/aibunny/dbt-4-superset/issues"
+        "Bug Tracker": "https://github.com/crafted-systems/smartcollect-lineage/issues"
     },
     license='MIT',
     packages=['toolbox'],
